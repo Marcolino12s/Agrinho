2a3894969210044bde01bc65fa20cdd553b86b61
Marcolino's <pereira.barbosa.marcos@escola.pr.gov.br>
2026-05-29 02:03:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+const botao = document.getElementById("explorarBtn");
 
+botao.addEventListener("click", () => {
+  document.getElementById("projetos").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+/* Efeito simples nos cards */
+
+const cards = document.querySelectorAll(".projeto-card");
+
+cards.forEach((card) => {
+
+  card.addEventListener("mouseenter", () => {
+    card.style.transform = "translateY(-10px)";
+  });
+
+  card.addEventListener("mouseleave", () => {
+    card.style.transform = "translateY(0)";
+  });
+
+});
